32d89abf555d5c5234a6ac930193fa5eeb6d9d38
Aleks <aleks@Admins-MacBook-Pro.local>
2020-08-12 11:51:12 -0400

    feat: Bubble sort, second smallest num

--- src/exercise-code.js
+++ src/exercise-code.js
@@ -1,5 +1,46 @@
 const ExerciseCode = () => {
-  console.log('Exercise Code output');
+  const inputs = [ [0], [0, 1], [4, 3, 2, 1], [9, 7, 8, 7] ];
+  const expectedOutputs = [0, 1, 2, 8];
+
+  const bubbleSort = arr => {
+    let swapped = false;
+    let counter = 1;
+
+    do {
+      swapped = false;
+      counter++;
+      for (let i=0; i<arr.length - 1; i++) {
+        if (arr[i] > arr[i+1]) {
+          [arr[i+1], arr[i]] = [arr[i], arr[i+1]];
+          swapped = true;        
+        }
+      }
+    } while(swapped)
+
+    console.log(`bubbleSort: [${arr}] was itereated ${counter} time(s)`);
+
+    return arr;
+  };
+
+  const secondSmallest = input => {
+    if (input.length < 2) return 0;
+
+    const inputSorted = bubbleSort(input);
+
+    let secondSmallestVal = inputSorted[0];
+    for (let i=1; i<inputSorted.length; i++) {
+      if (inputSorted[i] > secondSmallestVal) {
+        secondSmallestVal = inputSorted[i];
+        break;
+      }
+    }
+
+    return secondSmallestVal;
+  };
+
+  inputs.forEach((el, i) => {
+    console.log(`input: [${inputs[i]}], output: ${secondSmallest(el)}, expected: ${expectedOutputs[i]}`);
+  });
 
   return null;
 };
--- src/exercise-description.js
+++ src/exercise-description.js
@@ -3,8 +3,30 @@ import React from 'react';
 const ExerciseDescription = () => {
   return (
     <div>
-      <h2>Exercise Description</h2>
-      <p>Check browser console for the code output</p>
+      <h2>Problem 1 - Bubble Sort</h2>
+      <h3>Bubble sort runs in O(n²) time, fastest case is O(n) - linear - when input is already sorted</h3>
+      <p>
+        {'If the array has < 2 numbers then return 0, else return the second smallest number.'}
+        <br /><strong>Note: </strong>
+        In the above problem you cannot use the method <strong>sort()</strong> so please take a look at bubble sort or any other sort. 
+      </p>
+      <h3>Input</h3>
+      <p>[ [0], [0, 1], [4, 3, 2, 1] ]</p>
+      <h3>Expected Output</h3>
+      <p>[0, 1, 2]</p>
+      
+      <h3>Solution References</h3>
+      <div>
+        <a href="https://medium.com/javascript-algorithms/javascript-algorithms-bubble-sort-3d27f285c3b2">
+        https://medium.com/javascript-algorithms/javascript-algorithms-bubble-sort-3d27f285c3b2
+        </a>
+      </div>
+      <br />
+      <div>
+        <a href="https://www.w3resource.com/javascript-exercises/javascript-function-exercise-24.php">
+        https://www.w3resource.com/javascript-exercises/javascript-function-exercise-24.php
+        </a>
+      </div>
     </div>
   );
 };
